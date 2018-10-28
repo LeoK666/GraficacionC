70f405c388ada0ab0f01458de6ce97ddd5c99fe7
Leonardo Guzman Arellano <leo.guzman2507@hotmail.com>
2018-10-28 11:52:13 -0600

    Agregan un Hola mundo con printf

--- HolaMundo/HolaMundo.cpp
+++ HolaMundo/HolaMundo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+	printf("Hola Mundo"); //Hola mundo con printf
     return 0;
 }
