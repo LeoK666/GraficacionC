4c5447bf2783d4434245361a63256e64744fe8fb
Leonardo Guzman Arellano <leo.guzman2507@hotmail.com>
2018-11-11 16:10:59 -0600

    makefile

---
+++ HolaCabecera2/FuncionF.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "FuncionF.h"
+
+using namespace std;
+
+void hola(){
+	cout << "Hola Mundo" << endl;
+	cout << "El valor del saludo es" << saludo << endl;
+}
---
+++ MakeFile2/FuncionF.h
@@ -0,0 +1,7 @@
+#ifndef FUNCION_H_ //nombre del archivo nos indica que pasara si no esta definida la funcion, si la funcion ya esta definia no se vuelve a definir
+#define FUNCION_H_
+
+extern int saludo; //extern es para que no declare la variable varias veces en distintos archivos
+void hola();
+
+#endif //en los .h solo se declaran las funciones o las variables
